d7d4e32c3fa4d84ae5abd6bf3b4ea0e9672f88f1
Bhavya0020 <bhavya.learncode@gmail.com>
2021-06-29 10:44:06 +0530

    Make Product Component

--- readopolis.vue/src/views/Category.vue
+++ readopolis.vue/src/views/Category.vue
@@ -5,24 +5,10 @@
                 <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
             </div>
 
-            <!-- <ProductBox 
+            <ProductBox 
                 v-for="product in category.products"
                 v-bind:key="product.id"
-                v-bind:product="product" /> -->
-            <div
-                class="column is-3"
-                v-for="product in category.products"
-                v-bind:key="product.id"
-            >
-            <div class="box">
-                <figure class="image mb-4">
-                <img v-bind:src="product.get_thumbnail">
-            </figure>
-            <h3 class="is-size-4"> {{product.name}} </h3>
-            <p class="is-size-6 has-text-grey"> ₹ {{product.price}} </p>
-            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
-            </div>
-            </div>
+                v-bind:product="product" />
         </div>
     </div>
 </template>
@@ -30,11 +16,11 @@
 <script>
 import axios from 'axios'
 import { toast } from 'bulma-toast'
-// import ProductBox from '@/components/ProductBox'
+import ProductBox from '@/components/ProductBox'
 export default {
     name: 'Category',
     components: {
-        // ProductBox
+        ProductBox
     },
     data() {
         return {
--- readopolis.vue/src/views/Home.vue
+++ readopolis.vue/src/views/Home.vue
@@ -15,30 +15,17 @@
           <h2 class="is-size-2 has-text-centered">Latest Books</h2>
       </div>
 
-      <!-- <ProductBox 
+      <ProductBox 
         v-for="product in latestProducts"
         v-bind:key="product.id"
-        v-bind:product="product" /> -->
-      <div
-        class="column is-3"
-        v-for="product in latestProducts"
-        v-bind:key="product.id"
-      >
-      <div class="box">
-        <figure class="image mb-4">
-          <img v-bind:src="product.get_thumbnail">
-        </figure>
-        <h3 class="is-size-4"> {{product.name}} </h3>
-        <p class="is-size-6 has-text-grey"> ₹ {{product.price}} </p>
-        <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
-      </div>
-    </div>
+        v-bind:product="product" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
 
 
 export default {
@@ -49,6 +36,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted() {
     this.getLatestProducts()
@@ -74,11 +62,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  .image{
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
