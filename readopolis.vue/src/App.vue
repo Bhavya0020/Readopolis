ec9639b84273809dddd042c30c5ca3b3938eeb7d
Bhavya0020 <bhavya.learncode@gmail.com>
2021-06-29 10:59:00 +0530

    add Search Functionality

--- readopolis.vue/src/App.vue
+++ readopolis.vue/src/App.vue
@@ -10,6 +10,25 @@
         </a>
       </div>
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/buy" class="navbar-item">Buy</router-link>
           <router-link to="/rent" class="navbar-item">Rent</router-link>
